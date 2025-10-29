8c96a5489383dc29ff2ccb34a62ca6b90759e4d9
Abid Aghayev <106526739+scorpionabid@users.noreply.github.com>
2025-10-29 23:36:20 +0400

    user duzeldim

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './utils/authDebug' // Load auth debugging tools
 
 console.log('🚀 App starting with env:', import.meta.env.VITE_API_BASE_URL);
 
